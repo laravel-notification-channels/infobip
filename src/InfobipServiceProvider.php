75610219aa29e77c11e18dcf3db41e6bed25a5a5
Thomson Maguru <cliff@nyumbanitechnologies.com>
2020-08-12 06:54:11 +0300

    Add Auto-Discovery for Service Provider

--- src/InfobipServiceProvider.php
+++ src/InfobipServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace NotificationChannels\Infobip;
 
+use Illuminate\Notifications\ChannelManager;
+use Illuminate\Notifications\Notification;
 use Illuminate\Support\ServiceProvider;
 
 class InfobipServiceProvider extends ServiceProvider
@@ -13,6 +15,12 @@ class InfobipServiceProvider extends ServiceProvider
     {
         $this->app->bind(InfobipConfig::class, function () {
             return new InfobipConfig($this->app['config']['services.infobip']);
+        });
+
+        Notification::resolved(function (ChannelManager $service) {
+            $service->extend('infobip', function ($app) {
+                return $this->app->make(InfobipChannel::class);
+            });
         });
     }
 }
